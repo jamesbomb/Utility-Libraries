0d3465bec122b437aa1e56753b562629ba7e9d79
vinovator <vinovator@yahoo.co.in>
2016-03-16 15:29:00 +0000

    updated prompt password script

--- tk_PromptPassword.py
+++ tk_PromptPassword.py
@@ -11,7 +11,6 @@ import logging
 
 __author__ = "Vinoth Subramanian"
 __doc__ = "Reusable library which pops tkinter window to prompt password"
-__name__ = "tk_PromptPassword"
 
 
 """ Basic logging config
@@ -29,15 +28,26 @@ class prompt_password:
     Use tkinter to get password
     """
 
-    def __init__(self, title="TK Password Prompt"):
+    def __init__(self, title="TK Password Prompt", text="Password"):
         """
         Class initialization.
         Window title can be customized while instantiating the class.
         If title not provided, a default title is shown
+        If the prompt text is not provided, a default text is shown
         """
         self.root = tk.Tk()
         self.root.title(title)  # The title of tkinter window is configurable
-        self.root.minsize(width=300, height=125)
+        self.root.minsize(width=400, height=100)
+
+        # Define the frame to contain label and entry widget
+        self.messageFrame = tk.Frame(master=self.root)
+        self.messageFrame.grid(padx=2, pady=2)
+
+        # Define the frame for submit button
+        self.buttonFrame = tk.Frame(master=self.root)
+        self.buttonFrame.grid(padx=10, pady=2)
+
+        self.text = text
         self.password = None
 
     def submit_click(self, event=None):
@@ -61,13 +71,18 @@ class prompt_password:
         """
         Tkinter UI to prompt user for password
         """
-        password_lbl = tk.Label(self.root, text="Password", padx=2, pady=5)
-        self.password_entry = tk.Entry(self.root, show="*")
-        submit_button = tk.Button(self.root,
+        password_lbl = tk.Label(self.messageFrame,
+                                text=self.text,
+                                padx=5, pady=10)
+        self.password_entry = tk.Entry(self.messageFrame,
+                                       show="*")
+        submit_button = tk.Button(self.buttonFrame,
                                   text="Submit",
-                                  command=self.submit_click, width=20, pady=5)
+                                  padx=10, pady=5,
+                                  width=50,
+                                  command=self.submit_click)
 
-        password_lbl.grid(row=0, column=0, sticky="E")
+        password_lbl.grid(row=0, column=0)
         self.password_entry.grid(row=0, column=1)
         submit_button.grid(row=1, column=0, columnspan=2)
 
@@ -80,3 +95,9 @@ class prompt_password:
         self.root.mainloop()
 
         return self.password
+
+
+if __name__ == "__main__":
+    """ To invoke the library directly"""
+    ui = prompt_password()
+    ui.get_password()
